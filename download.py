001e1bdab341d4f38cf36871b5be6656491b1693
Yang Liu <liuyang12@focusmedia.cn>
2019-10-31 16:23:43 +0800

    update

--- download.py
+++ download.py
@@ -1,3 +1,4 @@
+import os
 import pickle
 from subprocess import Popen, PIPE
 
@@ -16,15 +17,20 @@ if __name__ == '__main__':
         after_file = tokens[1].strip()
 
         folder = 'data/photo'
-        process = Popen(["wget", '-N', before_file, "-P", folder], stdout=PIPE)
-        (output, err) = process.communicate()
-        exit_code = process.wait()
-        before_filename = before_file[before_file.rfind("/") + 1:]
 
-        process = Popen(["wget", '-N', after_file, "-P", folder], stdout=PIPE)
-        (output, err) = process.communicate()
-        exit_code = process.wait()
+        before_filename = before_file[before_file.rfind("/") + 1:]
+        before_filename = os.path.join(folder, before_filename)
+        if not os.path.isfile(before_filename):
+            process = Popen(["wget", '-N', before_file, "-P", folder], stdout=PIPE)
+            (output, err) = process.communicate()
+            exit_code = process.wait()
+
         after_filename = after_file[after_file.rfind("/") + 1:]
+        after_filename = os.path.join(folder, after_filename)
+        if not os.path.isfile(after_filename):
+            process = Popen(["wget", '-N', after_file, "-P", folder], stdout=PIPE)
+            (output, err) = process.communicate()
+            exit_code = process.wait()
 
         samples.append({'before_filename': before_filename, 'after_filename': after_filename})
 
