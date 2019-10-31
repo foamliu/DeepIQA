b31ed645354d281ed716087d80355cabc684ff40
Yang Liu <liuyang12@focusmedia.cn>
2019-10-31 18:05:00 +0800

    update

--- download.py
+++ download.py
@@ -18,20 +18,20 @@ if __name__ == '__main__':
         after_file = tokens[1].strip()
 
         before_filename = before_file[before_file.rfind("/") + 1:]
-        before_filename = os.path.join(folder, before_filename)
-        if not os.path.isfile(before_filename):
+        before_fullname = os.path.join(folder, before_filename)
+        if not os.path.isfile(before_fullname):
             process = Popen(["wget", '-N', before_file, "-P", folder], stdout=PIPE)
             (output, err) = process.communicate()
             exit_code = process.wait()
 
         after_filename = after_file[after_file.rfind("/") + 1:]
-        after_filename = os.path.join(folder, after_filename)
-        if not os.path.isfile(after_filename):
+        after_fullname = os.path.join(folder, after_filename)
+        if not os.path.isfile(after_fullname):
             process = Popen(["wget", '-N', after_file, "-P", folder], stdout=PIPE)
             (output, err) = process.communicate()
             exit_code = process.wait()
 
-        samples.append({'before_filename': before_filename, 'after_filename': after_filename})
+        samples.append({'before': before_filename, 'after': after_filename})
 
         with open('data.pkl', 'wb') as f:
             pickle.dump(samples, f)
